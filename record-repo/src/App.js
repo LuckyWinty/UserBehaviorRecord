4976af43a0b6dc5cb944a979975666237ae649e7
winty <18814127231@163.com>
2019-07-24 18:11:04 +0800

    start to record

---
+++ record-repo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import WrappedDemo from './component/FromTest'
+import './record'
+function App() {
+  return (
+    <div className="App">
+      <h3>要开始测试啦～</h3>
+      <WrappedDemo></WrappedDemo>
+    </div>
+  );
+}
+
+export default App;
